1f5db13b3f346fef9ef66fd7dc4a5494fdd57161
KaffuChino <47238156+daisukiKaffuChino@users.noreply.github.com>
2021-01-27 11:01:07 +0800

    add load animation

--- photoView.lua
+++ photoView.lua
@@ -7,7 +7,7 @@ import "com.coolapk.market.widget.photoview.PhotoView"
 --酷安的photoview太好用了
 
 activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-导航栏颜色(0xffffffff)
+导航栏颜色(0xff000000)
 
 pic=...
 
@@ -17,16 +17,47 @@ layout = {
   layout_width = "fill",
   layout_height = "fill",
   gravity = "center",
+  background = "#ff000000",
   {
-    PhotoView,
+    FrameLayout,
     layout_width = "fill",
     layout_height = "fill",
-    background = "#000000",
-    id = "mPhotoView",
+    {
+      LinearLayout,
+      layout_height = "fill",
+      layout_width = "fill",
+      gravity = "center",
+      background = "#ff000000",
+      id = "_PhotoView",
+      Visibility=0;
+      {
+        PhotoView,
+        layout_width = "fill",
+        layout_height = "fill",
+        background = "#000000",
+        id = "mPhotoView",
+      },
+    },
+    {
+      LinearLayout,
+      layout_height = "fill",
+      layout_width = "fill",
+      gravity = "center",
+      background = "#ff000000",
+      id = "_Progress",
+      Visibility=0;
+      {
+        ProgressBar,
+        id = "Progress",
+        layout_gravity = "center"
+      },
+    },
   },
 }
 
 activity.setContentView(loadlayout(layout))
+
+--Progress.IndeterminateDrawable.setColorFilter(PorterDuffColorFilter(转0x(primaryc), PorterDuff.Mode.SRC_ATOP))
 
 activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 --[
@@ -37,6 +68,18 @@ mPhotoView.setAdjustViewBounds(true)
 --]]
 import "com.bumptech.glide.*"
 import "com.bumptech.glide.load.engine.DiskCacheStrategy"
-Glide.with(this).load(pic).fitCenter().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE).into(mPhotoView)
+Glide.with(this)
+.load(pic)
+.listener({
+  onResourceReady=function()
+    _PhotoView.Visibility=0
+    _Progress.Visibility=8
+    return false
+  end,
+})
+.fitCenter()
+.skipMemoryCache(true)
+.diskCacheStrategy(DiskCacheStrategy.NONE)
+.into(mPhotoView)
 
 
