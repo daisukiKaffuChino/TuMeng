fa07d2d605cb9ca495ebe3f1a846fa1857f3f8cd
KaffuChino <47238156+daisukiKaffuChino@users.noreply.github.com>
2021-02-08 19:02:06 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,10 +1,16 @@
 appname="图萌Tumeng"
-appver="2.0"
-appcode="2000"
-packagename="com.tumeng.app"
+appver="2.0.1 Starkiller"
+appcode="200001"
+appsdk="15"
+path_pattern=""
+packagename="github.daisukiKaffuChino.TuMeng"
 theme="Theme_Material_Light_NoActionBar"
+app_key=""
+app_channel=""
+developer=""
+description=""
 debugmode=true
-welcome_time="900"
 user_permission={
-  "INTERNET"
+  "INTERNET",
+  "WRITE_EXTERNAL_STORAGE"
 }
